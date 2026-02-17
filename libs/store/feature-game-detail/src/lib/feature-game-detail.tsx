36342f8b40fa59532afc644f2d2d0edee6afd203
Denny <action79863@gmail.com>
2026-02-17 18:45:14 +0800

    lab 8: exhibit data to the routing page

--- libs/store/feature-game-detail/src/lib/feature-game-detail.tsx
+++ libs/store/feature-game-detail/src/lib/feature-game-detail.tsx
@@ -1,29 +1,93 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './feature-game-detail.module.scss';
 
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
+import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+import { formatRating } from '@bg-hoard/util-formatters';
 
 /* eslint-disable-next-line */
-export interface StoreFeatureGameDetailProps { }
+export interface StoreFeatureGameDetailProps {}
 
 export function StoreFeatureGameDetail(props: StoreFeatureGameDetailProps) {
+  const [state, setState] = useState<{
+    data: any;
+    loadingState: 'success' | 'error' | 'loading';
+  }>({
+    data: {},
+    loadingState: 'success',
+  });
   const params = useParams();
+
+  useEffect(() => {
+    setState({
+      ...state,
+      loadingState: 'loading',
+    });
+    const gameId = params['id'];
+    fetch(`/api/games/${gameId}`)
+      .then((x) => x.json())
+      .then((res) => {
+        setState({
+          ...state,
+          data: res,
+          loadingState: 'success',
+        });
+      })
+      .catch((err) => {
+        setState({
+          ...state,
+          loadingState: 'error',
+        });
+      });
+  }, [params['id']]);
+
   return (
     <div className={styles['container']}>
-      <Card>
-        <CardActionArea>
+      {state.loadingState === 'loading' ? (
+        'Loading...'
+      ) : state.loadingState === 'error' ? (
+        <div>Error fetching data</div>
+      ) : state.data == null ? (
+        ''
+      ) : (
+        <Card>
+          <CardHeader title={state.data.name}></CardHeader>
+          {state.data.image ? (
+            <CardMedia
+              className={styles['game-card-media']}
+              image={state.data.image}
+              title={state.data.name}
+            />
+          ) : null}
           <CardContent>
             <Typography variant="body2" color="textSecondary" component="p">
-              {params['id']}
+              {state.data.description}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              className={styles['game-rating']}
+            >
+              <strong>Rating:</strong> {formatRating(state.data.rating)}
             </Typography>
           </CardContent>
-        </CardActionArea>
-      </Card>
+          <CardActions>
+            <Button variant="contained" color="primary">
+              Buy
+            </Button>
+            <Button variant="contained">Share</Button>
+          </CardActions>
+        </Card>
+      )}
     </div>
   );
-};
+}
 
 export default StoreFeatureGameDetail;
