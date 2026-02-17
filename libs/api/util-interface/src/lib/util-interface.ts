1ba56396f156edbe053902fb7d63c6b8aa6e59b3
Denny <action79863@gmail.com>
2026-02-17 21:06:34 +0800

    lab 9: create interface and import in api app

--- libs/api/util-interface/src/lib/util-interface.ts
+++ libs/api/util-interface/src/lib/util-interface.ts
@@ -1,3 +1,12 @@
 export function utilInterface(): string {
   return 'util-interface';
 }
+
+export interface Game {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+  rating: number;
+}
