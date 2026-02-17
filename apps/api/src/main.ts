7979ff3950ef7ab2c868e1cd882a16d73e05555d
Denny <action79863@gmail.com>
2026-02-17 18:13:54 +0800

    lab 7: create games api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,17 +4,19 @@
  */
 
 import express from 'express';
-import * as path from 'path';
+import { getAllGames, getGame } from './app/games.repository';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.get('/api/games', (req, res) => {
+  res.send(getAllGames());
 });
 
-const port = process.env.PORT || 3333;
+app.get('/api/games/:id', (req, res) => {
+  return res.send(getGame(req.params.id));
+});
+
+const port = process.env.port || 3000;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
