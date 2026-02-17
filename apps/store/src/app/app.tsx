bb8e25a4ddb206c0a48a076f4dcecc293d29d255
Denny <action79863@gmail.com>
2026-02-17 16:28:02 +0800

    lab 6: generate a route lib

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -9,6 +9,10 @@ import Typography from '@mui/material/Typography';
 import { Header } from '@bg-hoard/ui-shared';
 import { formatRating } from '@bg-hoard/util-formatters';
 
+import { Route, Routes, Link } from 'react-router-dom';
+
+import { FeatureGameDetail } from '@bg-hoard/feature-game-detail';
+
 export const App = () => {
   return (
     <>
@@ -27,7 +31,11 @@ export const App = () => {
                   <Typography gutterBottom variant="h5" component="h2">
                     {x.name}
                   </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
                     {x.description}
                   </Typography>
                   <Typography
@@ -44,6 +52,47 @@ export const App = () => {
           ))}
         </div>
       </div>
+
+      {/* START: routes */}
+      {/* These routes and navigation have been generated for you */}
+      {/* Feel free to move and update them to fit your needs */}
+      <br />
+      <hr />
+      <br />
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/feature-game-detail">FeatureGameDetail</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route.{' '}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route path="/feature-game-detail" element={<FeatureGameDetail />} />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
     </>
   );
 };
---
+++ libs/store/feature-game-detail/src/lib/feature-game-detail.tsx
@@ -0,0 +1,11 @@
+import styles from './feature-game-detail.module.scss';
+
+export function FeatureGameDetail() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to FeatureGameDetail!</h1>
+    </div>
+  );
+}
+
+export default FeatureGameDetail;
